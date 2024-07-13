0b990e01e02393e6bdc88ca0352ae86637ab854b
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-07-13 12:06:33 -0300

    Feat: Add middleware to authenticate session token and user externalId

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -3,7 +3,8 @@ import {z} from 'zod'
 const envSchema = z.object({
     PORT: z.string().default('3000'),
     DATABASE_URL: z.string().url(),
-    WEBHOOK_SECRET: z.string()
+    WEBHOOK_SECRET: z.string(),
+    JWT_PUBLIC_KEY:z.string().url()
 })
 
 export const env = envSchema.parse(process.env); 
