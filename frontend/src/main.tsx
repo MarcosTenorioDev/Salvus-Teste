cc97fcede74e29320df37794bddd2709cfede2be
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-07-15 12:32:57 -0300

    Feat: add clerkProvider context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ptBR } from "@clerk/localizations";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+	throw new Error("Missing Publishable Key");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ClerkProvider
+			publishableKey={PUBLISHABLE_KEY}
+			appearance={{
+				variables: { colorPrimary: "green" },
+			}}
+			localization={ptBR}
+		>
+			<App />
+		</ClerkProvider>
+	</React.StrictMode>
+);
