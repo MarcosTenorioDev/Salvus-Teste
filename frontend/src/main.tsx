827340e6a5fdd47d4aaace72e672e3d4019fdd0d
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-07-16 17:41:52 -0300

    Feat: created toast service

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ptBR } from "@clerk/localizations";
 import { LangProvider } from "./assets/i18n/index.tsx";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
@@ -13,6 +15,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
+		<ToastContainer className="z-[999999999999999999999]"/>
 		<ClerkProvider
 			publishableKey={PUBLISHABLE_KEY}
 			appearance={{
